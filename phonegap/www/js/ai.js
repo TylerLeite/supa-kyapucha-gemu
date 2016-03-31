a8cfa9d95515d01a87c315c7e3508045e7b1db3b
Tyler Leite <tyler.leite@yandex.com>
2016-03-31 03:11:39 -0400

    made AI a bit smarter
    Signed-off-by: Tyler Leite <tyler.leite@yandex.com>

--- phonegap/www/js/ai.js
+++ phonegap/www/js/ai.js
@@ -9,10 +9,54 @@ GT.AI = function(board) {
 	this.gamestate = board;
 };
 
+GT.AI.prototype.filterCorners = function(moves) {
+	var corners = [];
+	for (var i = 0; i < moves.length; i++){
+		var mv = moves[i];
+		if ((mv[0] == 0 || mv[0] == this.gamestate.wdt()-1) &&
+					(mv[1] == 0 || mv[1] == this.gamestate.hgt()-1)){
+			corners.push(mv);
+		}
+	}
+
+	return corners;
+};
+
+GT.AI.prototype.filterEdges = function(moves) {
+	var edges = [];
+	for (var i = 0; i < moves.length; i++){
+		var mv = moves[i];
+		if (mv[0] == 0 || mv[0] == this.gamestate.wdt()-1 ||
+					mv[1] == 0 || mv[1] == this.gamestate.hgt()-1){
+			edges.push(mv);
+		}
+	}
+
+	return edges;
+};
+
 GT.AI.prototype.makeMove = function() {
 	var legalMoves = this.gamestate.getEmptySquares();
-	var rand = Math.floor(Math.random() * legalMoves.length);
-	var out = legalMoves[rand].split("");
+	var edges = this.filterEdges(legalMoves);
+	var corners = [];
+	if (edges.length > 0){
+		corners = this.filterCorners(edges);
+	}
+
+	var rand; var out;
+	if (corners.length > 0) {
+		rand = Math.floor(Math.random() * corners.length);
+		out = corners[rand].split("");
+	} else if (edges.length > 0){
+		rand = Math.floor(Math.random() * edges.length);
+		out = edges[rand].split("");
+	} else if (legalMoves.length > 0){
+		rand = Math.floor(Math.random() * legalMoves.length);
+		out = legalMoves[rand].split("");
+	} else {
+		out = ['-1', '-1'];
+	}
+
 	out[0] = parseInt(out[0]);
 	out[1] = parseInt(out[1]);
 	return out;
