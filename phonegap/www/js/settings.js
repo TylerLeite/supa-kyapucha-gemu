892d346591d2a7bff6ccca1b55676c42c276621a
Kevin Stoner <stonek4@rpi.edu>
2016-03-31 20:20:23 -0400

    implemented use of settings (not checked yet)

--- phonegap/www/js/settings.js
+++ phonegap/www/js/settings.js
@@ -3,7 +3,7 @@ if (GT === null || typeof(GT) != "object"){ var GT = new Object();}
 GT.Player = function(name, piece) {
 	this.name = name;
 	this.piece = piece;
-}
+};
 
 GT.Settings = function() {
 	this.players = [];
@@ -13,35 +13,41 @@ GT.Settings = function() {
 
 	this.addPlayer('Human', '4minute');
 	this.addPlayer('CPU', 'kara');
-}
+};
 
 GT.Settings.prototype.addPlayer = function(name, tile) {
 	this.numPlayers += 1;
-	piece = 'img/pieces/' + tile + '.png';
+	piece = '../img/pieces/' + tile + '.png';
 	player = new Player(name, piece);
 	this.players.push(player);
-}
+};
 
-GT.Settings.prototype.changePiece(player, tile) {
+GT.Settings.prototype.changePiece = function(player, tile) {
 	if (player > numPlayers || player <= 0){
 		return;
 	} else {
 		this.players[player-1].tile = tile;
 	}
-}
+};
 
-GT.Settings.prototype.changeName(player, name) {
+GT.Settings.prototype.changeName = function(player, name) {
 	if (player > numPlayers || player <= 0){
 		return;
 	} else {
 		this.players[player-1].name = name;
 	}
-}
+};
 
 GT.Settings.prototype.setSinglePlayer = function() {
 	this.singlePlayer = true;
-}
+};
 
 GT.Settings.prototype.setMultiPlayer = function() {
 	this.singlePlayer = false;
-}
+};
+GT.Settings.prototype.getPiece = function(player) {
+	return players[player].piece();
+};
+GT.Settings.prototype.getName = function(player) {
+	return players[player].name();
+};
