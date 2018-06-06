a3ce6b36d6b809a557f7fdc65911d092d4af2a1f
Kevin Stoner <kstoner@inovonics.com>
2018-06-05 19:07:29 -0600

    logic working, technically playable at this point haha

--- webapp/src/board/board.ts
+++ webapp/src/board/board.ts
@@ -1,5 +1,5 @@
 import { bindable, LogManager } from 'aurelia-framework';
-import { Tile } from '../tile/tile';
+import { Tile, States } from '../tile/tile';
 
 const logger = LogManager.getLogger('board');
 
@@ -7,14 +7,74 @@ export class Board {
     @bindable public height: number;
     @bindable public width: number;
     public tiles: Array<Tile>[] = new Array<Tile[]>();
+    private turn: States = States.PLAYER1;
 
-    bind() {
+    public bind() {
         for (let i = 0; i < this.height; i++) {
             this.tiles.push(new Array<any>(this.width));
         }
     }
 
-    place(x: number, y: number) {
-        this.tiles[x][y].flip();
+    public place(x: number, y: number) {
+        if (this.tiles[y][x].getState() !== States.EMPTY) {
+            return;
+        }
+
+        this.tiles[y][x].setState(this.turn);
+
+        for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+                this.checkReversi(x, y, i, j);
+            }
+        }
+
+        if (this.turn === States.PLAYER1) {
+            this.turn = States.PLAYER2;
+        } else {
+            this.turn = States.PLAYER1;
+        }
     }
+
+    public reset() {
+        for (let i = 0; i < this.height; i++) {
+            for (let j = 0; j < this.width; i++) {
+                this.tiles[i][j].reset();
+            }
+        }
+    }
+
+    public inBounds(x, y) {
+        if (x >= this.width || x < 0) {
+            return false;
+        }
+        if (y >= this.height || y < 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public checkReversi(sx, sy, xdir, ydir) {
+        const nx = sx + xdir;
+        const ny = sy + ydir;
+
+        if (!this.inBounds(nx, ny)) {
+            return false;
+        } else if (this.tiles[ny][nx].getState() === States.EMPTY) {
+            return false;
+        } else if (this.tiles[ny][nx].getState() === this.turn) {
+            if (this.tiles[sy][sx].getState() !== this.turn) {
+                this.tiles[sy][sx].setState(this.turn); // Comment this out if just checking
+            }
+            return true;
+        } else {
+            if (this.checkReversi(nx, ny, xdir, ydir)) {
+                if (this.tiles[sy][sx].getState() !== this.turn) {
+                    this.tiles[sy][sx].setState(this.turn); // Comment this out if just checking
+                }
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
 }
--- webapp/src/tile/tile.ts
+++ webapp/src/tile/tile.ts
@@ -1,13 +1,66 @@
-export class Tile {
-    private state: TileStates = TileStates.EMPTY;
-    public tileUi: HTMLElement;
+import { LogManager } from 'aurelia-framework';
 
-    public flip() {
-        this.tileUi.classList.toggle('is-flipped');
+const logger = LogManager.getLogger('tile');
+
+export class Tile {
+    private state: States = States.EMPTY;
+    public tileUi: HTMLElement;
+    public tileFrontUi: HTMLElement;
+    public tileBackUi: HTMLElement;
+
+    private emptyColor = "grey";
+    private player1Color = "red";
+    private player2Color = "blue";
+
+    private flipClass = 'is-flipped';
+
+
+
+    public attached() {
+        this.tileFrontUi.style.backgroundColor = this.emptyColor;
+    }
+
+    private flip() {
+        this.tileUi.classList.toggle(this.flipClass);
+    }
+
+    private isFlipped() {
+        return this.tileUi.classList.contains(this.flipClass);
+    }
+
+    public getState() {
+        return this.state;
+    }
+
+    public setState(newState: States) {
+        if (newState === this.state) {
+            return;
+        }
+        if (this.state === States.EMPTY) {
+            if (newState === States.PLAYER1) {
+                this.tileFrontUi.style.backgroundColor = this.player1Color;
+                this.tileBackUi.style.backgroundColor = this.player2Color;
+            } else if (newState === States.PLAYER2) {
+                this.tileFrontUi.style.backgroundColor = this.player2Color;
+                this.tileBackUi.style.backgroundColor = this.player1Color;
+            }
+        } else {
+            this.flip();
+        }
+        this.state = newState;
+    }
+
+    public reset() {
+        this.state = States.EMPTY;
+        this.tileFrontUi.style.backgroundColor = this.emptyColor;
+        this.tileBackUi.style.backgroundColor = this.emptyColor;
+        if (this.isFlipped()) {
+            this.flip();
+        }
     }
 }
 
-export enum TileStates {
+export enum States {
     PLAYER1,
     PLAYER2,
     EMPTY
