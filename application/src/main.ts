d40f7a17a0b34114bd8d9cd74a372767c78d289c
Kevin Stoner <kstoner@inovonics.com>
2018-08-09 15:35:32 -0600

    fixed some linting errors

--- application/src/main.ts
+++ application/src/main.ts
@@ -2,17 +2,23 @@ import { Aurelia } from 'aurelia-framework';
 import environment from './environment';
 
 export function configure(aurelia: Aurelia) {
-  aurelia.use
+    aurelia.use
     .standardConfiguration()
     .feature('resources');
 
-  if (environment.debug) {
-    aurelia.use.developmentLogging();
-  }
+    if (environment.debug) {
+        aurelia.use.developmentLogging();
+    }
 
-  if (environment.testing) {
-    aurelia.use.plugin('aurelia-testing');
-  }
+    if (environment.testing) {
+        aurelia.use.plugin('aurelia-testing');
+    }
 
-  aurelia.start().then(() => aurelia.setRoot('app/app'));
+    aurelia.start()
+    .then(() => {
+        return aurelia.setRoot('app/app');
+    })
+    .catch((reason: any) => {
+        console.error(`Wow you really messed something up bad: ${reason}`);
+    });
 }
