0cf56dc6d478645c050b75b179a924b86e4a9a2a
Kevin Stoner <kstoner@inovonics.com>
2018-08-16 17:41:39 -0600

    added a blank page for level selection

--- application/src/adventure/adventure.ts
+++ application/src/adventure/adventure.ts
@@ -1,3 +1,16 @@
-export class Adventure {
+import { LogManager } from 'aurelia-framework';
+import { RouterConfiguration } from 'aurelia-router';
 
+const logger = LogManager.getLogger('adventure');
+
+export class Adventure {
+    configureRouter(config: RouterConfiguration): void {
+        logger.debug("configuring the router");
+        config.title = "SKG - Adventure";
+        config.map([
+            { route: '', redirect: 'levels' },
+            { route: 'levels', name: 'levels', moduleId: 'levels/levels' }
+        ]);
+        config.mapUnknownRoutes('levels/levels');
+    }
 }
