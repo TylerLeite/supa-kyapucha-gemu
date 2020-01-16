d6a2f57f72b72818b70650a8ce73a360fe9c348e
Kevin Stoner <stonek4rpi@gmail.com>
2020-01-16 16:51:35 -0700

    Added comment

--- application/src/board/boards/ai-board.ts
+++ application/src/board/boards/ai-board.ts
@@ -35,6 +35,7 @@ export class AiBoard extends Board {
     public attached(): void {
         //tslint:disable-next-line
         if (this.boardUi === undefined) {
+            // This is a hack until I can figure out how to pass the board reference in
             this.boardUi = <HTMLElement>document.getElementsByClassName("board")[0];
         }
         this.layoutChanged();
